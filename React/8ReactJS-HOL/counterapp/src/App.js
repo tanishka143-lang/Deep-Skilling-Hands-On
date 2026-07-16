bb90ba7fdf77b2b82cd7677dda80909228c42507
Tanny Candy <tannycandy143@gmail.com>
2026-07-17 03:46:15 +0530

    ADDED 8

---
+++ React/8ReactJS-HOL/counterapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CountPeople from "./components/CountPeople";
+
+function App() {
+  return (
+    <div>
+      <CountPeople />
+    </div>
+  );
+}
+
+export default App;
