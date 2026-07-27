a1f47445d56ed42093cbc1c26e54d0ca727bcc11
Tanny Candy <tannycandy143@gmail.com>
2026-07-27 20:29:33 +0530

    Added student-course-portal

---
+++ AngularHandson/student-course-portal/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { Header } from './components/header/header';
+import { Home } from './pages/home/home';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Header, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
